5d7b9a3aa2685a217920d6c0e2d6172bd02ae1a6
Michal Ziulek <michalziulek@gmail.com>
2020-11-17 13:07:47 +0100

    -

--- Source/Pacman/PacmanPawn.cpp
+++ Source/Pacman/PacmanPawn.cpp
@@ -102,7 +102,7 @@ void APacmanPawn::Tick(float DeltaTime)
 
 void APacmanPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	APawn::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAction(TEXT("MoveUp"), IE_Pressed, this, &APacmanPawn::MoveUp);
 	PlayerInputComponent->BindAction(TEXT("MoveDown"), IE_Pressed, this, &APacmanPawn::MoveDown);
